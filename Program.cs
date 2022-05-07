d1ca1ad20e327ee1f365b38d18b8be17fde52420
Tim Jones <timothy.jones@gmx.us>
2022-05-07 15:55:59 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using DesignPatterns2022.Singleton;
+
+Singleton object1 = Singleton.Instance();
+Singleton object2 = Singleton.Instance();
+
+if(object1 == object2)
+{
+    Console.WriteLine("These objects are the same");
+
+}
+
+int val = 0;
